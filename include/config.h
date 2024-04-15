7dbabf38f1819e122d982a26cc54c47d357f58e5
simonttp78 <simonttp78@gmail.com>
2024-04-15 16:21:25 +0200

    master adaptation: simulate to false, debug to 0

--- include/config.h
+++ include/config.h
@@ -24,7 +24,7 @@
 
 #define FW_VER "1.16.0  "         // Flyball ETS firmware version
 
-#define Simulate true             // Set to true to enable race simulation (see Simulator.h/.cpp)
+#define Simulate false            // Set to true to enable race simulation (see Simulator.h/.cpp)
 #define NumSimulatedRaces 60      // Number of prepeared simulated races. Sererial interface command to change interface: e.g. "race 1"
 #define TRIGGER_QUEUE_LENGTH 110  // Number of triggers in the queue
 
